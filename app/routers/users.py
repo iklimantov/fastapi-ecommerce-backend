8a4d4c307ee9b1b2b30e90822890c9880b6c36a5
Ilya Klimantov <i.v.klimantov2000@gmail.com>
2026-02-07 19:38:32 +0300

    Configure JWT Authorization

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,13 +10,13 @@ from app.db_depends import get_async_db
 from app.models.users import User as UserModel
 from app.schemas import User as UserSchema
 from app.schemas import UserCreate
+from app.auth import verify_password, create_access_token
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.post("/", response_model=UserSchema, status_code=status.HTTP_201_CREATED)
-async def create_user(
-    user: UserCreate, db: Annotated[AsyncSession, Depends(get_async_db)]
+async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_async_db)]
 ):
     """
     Регистрирует нового пользователя с ролью 'buyer' или 'seller'.
@@ -38,3 +39,22 @@ async def create_user(
     await db.commit()
     await db.refresh(db_user)
     return db_user
+
+
+@router.post('/token')
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                db: Annotated[AsyncSession, Depends(get_async_db)]):
+    """
+    Аутентифицирует пользователя и возвращает JWT с email, role и id.
+    """
+    result = await db.scalars(
+        select(UserModel).where(UserModel.email == form_data.username, UserModel.is_active == True))
+    user = result.first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user.email, "role": user.role, "id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
